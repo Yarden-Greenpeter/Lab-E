5b32f0c31d55385afeffba8b0bc32ef9e9c8ad20
Yarden-Greenpeter <ygreenpeter@gmail.com>
2024-07-09 20:59:04 +0300

    part 2 started need to be tested

--- myELF.c
+++ myELF.c
@@ -66,7 +66,7 @@ void print_elf_info(state *s) {
 }
 
 int find_file_index(file_stack* stack, const char* file_name) {
-    for (int i = 0; i < MAX_FILES; ++i) {
+    for (int i = 0; i < MAX_FILES; i++) {
         if (strcmp(stack->files[i].file_name, file_name) == 0 && stack->valid_state_files[i] == 1) {
             return i;
         }
@@ -91,7 +91,7 @@ void examine_ELF_file(file_stack* stack) {
 
     // Find a place to store the new file information
     int empty_index = -1;
-    for (int i = 0; i < MAX_FILES; ++i) {
+    for (int i = 0; i < MAX_FILES; i++) {
         if (stack->valid_state_files[i] == 0) {
             empty_index = i;
             break;
@@ -159,7 +159,7 @@ void examine_ELF_file(file_stack* stack) {
 // PART 1
 // Function to print section names
 void print_section_names(file_stack* stack) {
-    for (int i = 0; i < MAX_FILES; ++i) {
+    for (int i = 0; i < MAX_FILES; i++) {
         if (stack->valid_state_files[i] == 1) {
             state* s = &stack->files[i];
             Elf32_Shdr* sh_table = (Elf32_Shdr*)(s->map_start + s->sh_offset);
@@ -170,25 +170,70 @@ void print_section_names(file_stack* stack) {
 
             printf("[index] section_name section_address section_offset section_size  section_type\n");
             
-            for (int j = 0; j < s->sh_num; ++j) {
+            for (unsigned int j = 0; j < s->sh_num; j++) {
                 printf("[%d] %s 0x%x %d %d  %d\n", j, &shstrtab[sh_table[j].sh_name], sh_table[j].sh_addr, sh_table[j].sh_offset, sh_table[j].sh_size, sh_table[j].sh_type);
             }
         } else {
-            break;  // Stop iterating if encounter an invalid file state
+            break;  // Stop  if encounter an invalid file state
+        }
+    }
+}
+//------------------------------------------------------------------------------------
+//Part 2
+// Function to print symbols
+void print_symbols(file_stack* stack) {
+    for (int i = 0; i < MAX_FILES; i++) {
+        if (stack->valid_state_files[i] == 1) {
+            state* s = &stack->files[i];
+            Elf32_Shdr* sh_table = (Elf32_Shdr*)(s->map_start + s->sh_offset);
+            Elf32_Shdr* symtab_hdr = NULL;
+            Elf32_Shdr* strtab_hdr = NULL;
+            char* strtab = NULL;
+            char* shstrtab = NULL;
+
+            // Find the symbol table and corresponding string table section headers
+            for (unsigned int  j = 0; j < s->sh_num; j++) {
+                if (sh_table[j].sh_type == SHT_SYMTAB) {
+                    symtab_hdr = &sh_table[j];
+                } else if (sh_table[j].sh_type == SHT_STRTAB && strcmp(".strtab", (char*)(s->map_start + sh_table[j].sh_name)) == 0) {
+                    strtab_hdr = &sh_table[j];
+                    strtab = (char*)(s->map_start + strtab_hdr->sh_offset);
+                }
+            }
+
+            if (symtab_hdr == NULL || strtab_hdr == NULL || strtab == NULL) {
+                printf("Symbol table or string table not found in %s\n", s->file_name);
+                continue;
+            }
+
+            // Calculate the number of symbols
+            int num_symbols = symtab_hdr->sh_size / sizeof(Elf32_Sym);
+            Elf32_Sym* symtab = (Elf32_Sym*)(s->map_start + symtab_hdr->sh_offset);
+
+            // Print header
+            printf("File %s\n", s->file_name);
+            printf("[index] value section_index section_name symbol_name\n");
+
+            // Print each symbol
+            for (int k = 0; k < num_symbols; k++) {
+                printf("[%d] 0x%08x %d %s %s\n", k, symtab[k].st_value, symtab[k].st_shndx, &shstrtab[symtab[k].st_shndx], &strtab[symtab[k].st_name]);
+            }
+        } else {
+            break;  // Stop if encounter an invalid file state
         }
     }
 }
 
+//------------------------------------------------------------------------------------------
 
 void not_implemented(file_stack* stack) {
     printf("Not implemented yet\n");
 }
 
 void quit(file_stack* stack) {
-    for (int i = 0; i < MAX_FILES; ++i) {
+    for (int i = 0; i < MAX_FILES; i++) {
         if (stack->valid_state_files[i] == 1) {
             munmap(stack->files[i].map_start, sizeof(Elf32_Ehdr));
-            stack->valid_state_files[i] = 0;
         }
     }
     exit(0);
@@ -200,7 +245,7 @@ int main(void) {
         {"Toggle Debug Mode", toggle_debug_mode_file_stack},
         {"Examine ELF File", examine_ELF_file},
         {"Print Section Names", print_section_names},
-        {"Print Symbols", not_implemented},
+        {"Print Symbols", print_symbols},
         {"Check Files for Merge", not_implemented},
         {"Merge ELF Files", not_implemented},
         {"Quit", quit},
